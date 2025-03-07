2c573fbc8db259b445df5c775e3157cee66967f8
johnbhd <villegasjohnb18@gmail.com>
2025-03-07 18:20:46 +0800

    Day 4 50% done need the function and responsive

---
+++ Day 4/multiplication-table-generator/app.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('Myform');
+const results = document.getElementById('results');
+const tbody = document.getElementById('tbody');
+
+const Result = (InputNum, RangeNum, operator) => {
+    const num1 = Number(InputNum);
+    const num2 = Number(RangeNum);
+
+    const operation = {
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        '/': num2 !== 0 ? (num1 / num2).toFixed(2) : 'error'
+    }
+    const calculation = operation[operator] ?? 'Invalid';
+
+    const calResult = `<tr>
+        <td>${num1}</td>
+        <td>${operator}</td>
+        <td>${num2}</td>
+        <td>=</td>
+        <td>${calculation}</td>
+     </tr>;`
+
+    tbody.insertAdjacentHTML('beforeend', calResult);
+}
+
+const Calculation = (event) => {
+    event.preventDefault();
+
+    const InputNum = document.getElementById('InputNum').value;
+    const RangeNum = document.getElementById('RangeNum').value;
+    const operator = document.getElementById('select').value;
+
+    Result(InputNum, RangeNum, operator);
+}
+
+form.addEventListener('submit', Calculation);
