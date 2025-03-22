2e7f5237faddf6420fe09eb4cf2ef4c082ba063d
johnbhd <villegasjohnb18@gmail.com>
2025-03-22 18:02:11 +0800

    commit

--- Day 4/number-guessing/script.js
+++ Day 4/number-guessing/script.js
@@ -1,1 +1,20 @@
-fafa
+// range
+const rangeBut = document.getElementById('rangeBut');
+const startnum = document.getElementById('startNum');
+const endnum = document.getElementById('endNum');
+// output
+const submit = document.getElementById('submit');
+const restart = document.getElementById('restart');
+const guess = document.getElementById('guess');
+
+// range click
+
+const rangeToggle = () => {
+    let startval = parseInt(startnum.value);
+    let endval = parseInt(endnum.value);
+
+    guess.placeholder = `Enter a Number(${startval}-${endval})`;
+
+}
+
+rangeBut.addEventListener('click', rangeToggle);
