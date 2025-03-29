8cf2feff2b14c98607af8e6aa1f5aedbef68c952
johnbhd <villegasjohnb18@gmail.com>
2025-03-29 18:41:07 +0800

    Day 5 fibonacci generator. Jesus loves you - Psalms 119:105

--- Day 5/Fibonacci_Sequence/app.js
+++ Day 5/Fibonacci_Sequence/app.js
@@ -1,1 +1,27 @@
-afa
+const generate = document.getElementById('generate');
+const startNum = document.getElementById('startNum');
+const endNum = document.getElementById('endNum');
+
+const result = document.getElementById('result');
+
+function calculation(n, z) {
+    let sequence = [];
+
+    for (let i = 0, a = 0, b = 1; i <= z; i++) {
+        if (i >= n) sequence.push(a); 
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return sequence.length > 0 ? sequence.join(', ') : "No fibonacci numbers in range";
+}
+
+function Result() {
+    let startval = parseInt(startNum.value);
+    let endval = parseInt(endNum.value);
+
+    result.textContent = calculation(startval, endval); 
+}
+
+generate.addEventListener('click', Result);
