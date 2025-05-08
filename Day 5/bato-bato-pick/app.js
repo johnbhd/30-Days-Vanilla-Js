8026ae726a486895fed55a94e35fad3f5d04bb81
johnbhd <villegasjohnb18@gmail.com>
2025-05-08 18:33:34 +0800

    little bit logic need to do and design. Jesus loves you - Jeremiah 31:3

--- Day 5/bato-bato-pick/app.js
+++ Day 5/bato-bato-pick/app.js
@@ -1,13 +1,51 @@
 const player = document.getElementById('player');
 const computer = document.getElementById('computer');
-
-const choices = [...document.querySelectorAll('button')];
+const choices = document.querySelector('.choices');
 const title = document.querySelector('h2');
 
+const pickChoices = Choices();
+
+function Choices() {
+    const pick = [
+        {
+            choice: "bato",
+            img: ["https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/rock1.png", "https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/rock2.png"]
+        },
+        {
+            choice: "papel",
+            img: ["https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/paper1.png", "https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/paper2.png"]
+        },
+        {
+            choice: "gunting",
+            img: ["https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/scissor1.png", "https://raw.githubusercontent.com/jbmvdev/img-host/main/pick/scissor2.png"]
+        }
+    ];
+  return pick;
+}
+
+pickChoices.forEach((picker, index) => {
+    choices.innerHTML += `
+           <img src=${picker.img[0]} alt=${picker.choice} onclick=''>
+    `;
+    console.log(picker.img[0])
+    console.log(picker.choice+ index);
+    
+    
+});
+
+function getResult(playerChoice, computerChoice) {
+    if (playerChoice ===  computerChoice) return "Tie";
+    if ((playerChoice + 1) % 3 === computerChoice) return "Computer Wins";
+    return "Player Wins"
+}
+console.log(getResult(1, 2));
+
+
+
+/*
 function Referee(playerchoices, computerChoice) {
     const playerPick = playerchoices;
     const computerPick = computerChoice.textContent;
-    
     
     player.textContent = playerPick;
     computer.textContent = computerPick;
@@ -39,5 +77,4 @@ choices.forEach(but => {
     
     });
 });
-
-
+*/
