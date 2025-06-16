c880ec40e0f8fb20845baa3cffdf84ff877c5185
JB Villegas <villegasjohnb18@gmail.com>
2025-06-16 22:46:37 +0800

    zoro animation. Jesus loves you - Jeremaiah 33:3

---
+++ Day 5/one-piece-game/src/js/zoro.js
@@ -0,0 +1,34 @@
+const zoro = document.querySelector(".zoro");
+const moves = [
+  document.querySelector(".zoro-move1"),
+  document.querySelector(".zoro-move2"),
+  document.querySelector(".zoro-move3"),
+  document.querySelector(".zoro-move4"),
+  document.querySelector(".zoro-move5")
+];
+const sizeCon = [
+  { height: "190px" },
+  { height: "190px", width: "270px" },
+  { width: "270px", height: "190px" },
+  { width: "222px", height: "190px" },  
+  { width: "260px", height: "190px" }
+];
+
+let current = 0;
+moves[current].addEventListener("animationend", movements);
+
+function movements() {
+  moves[current].style.display = "none";
+  current++;
+
+  if (moves[current]) {
+    moves[current].style.display = "block";
+    void moves[current].offsetWidth; 
+    
+    if (sizeCon[current]) {
+      Object.assign(zoro.style, sizeCon[current])
+    }
+    
+    moves[current].addEventListener("animationend", movements, { once: true });
+  }
+}
