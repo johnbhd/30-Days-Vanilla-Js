6752db9c56276d8251063a464ac206ff047bfecc
johnbhd <villegasjohnb18@gmail.com>
2025-06-17 12:34:09 +0800

    fixing zoro. Jesus loves you Philippians 4:13

--- Day 5/one-piece-game/src/js/zoro.js
+++ Day 5/one-piece-game/src/js/zoro.js
@@ -4,14 +4,20 @@ const moves = [
   document.querySelector(".zoro-move2"),
   document.querySelector(".zoro-move3"),
   document.querySelector(".zoro-move4"),
-  document.querySelector(".zoro-move5")
+  document.querySelector(".zoro-move5"),
+  document.querySelector(".zoro-move6"),
+  document.querySelector(".zoro-move7"),
+  document.querySelector(".zoro-move8")
 ];
 const sizeCon = [
   { height: "190px" },
   { height: "190px", width: "270px" },
   { width: "270px", height: "190px" },
-  { width: "222px", height: "190px" },  
-  { width: "260px", height: "190px" }
+  { width: "225px", height: "190px" },  
+  { width: "260px", height: "190px" },
+  { width: "190px", height: "170px" },
+  { width: "270px" },
+  { width: "190px", height: "252px" },
 ];
 
 let current = 0;
