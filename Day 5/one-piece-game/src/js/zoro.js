bcaf2d00d539cd5ea0df14377a32813bac6813f4
johnbhd <villegasjohnb18@gmail.com>
2025-06-19 15:11:18 +0800

    zoro move. Jesus loves you - Matthew 11:28

--- Day 5/one-piece-game/src/js/zoro.js
+++ Day 5/one-piece-game/src/js/zoro.js
@@ -7,17 +7,21 @@ const moves = [
   document.querySelector(".zoro-move5"),
   document.querySelector(".zoro-move6"),
   document.querySelector(".zoro-move7"),
-  document.querySelector(".zoro-move8")
+  document.querySelector(".zoro-move8"),
+  document.querySelector(".zoro-move9"), 
+  document.querySelector(".zoro-move10")
 ];
 const sizeCon = [
   { height: "190px" },
-  { height: "190px", width: "270px" },
+  { height: "180px", width: "267px" },
   { width: "270px", height: "190px" },
   { width: "225px", height: "190px" },  
   { width: "260px", height: "190px" },
   { width: "190px", height: "170px" },
-  { width: "270px" },
-  { width: "190px", height: "252px" },
+  { width: "270px", height: "198px"},
+  { width: "190px", height: "248px" },
+  { width: "150px", height: "240px" },
+  { width: "297px", height: "200px" },
 ];
 
 let current = 0;
